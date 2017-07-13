33096f74f604603ccf2465819193589672da2160
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2017-07-13 18:59:29 +0200

    Initial commit
    Placing objects with physics using Physics.Simulate in Unity2017

---
+++ Assets/Editor/PhysicsSimulationEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PhysicsSimulation))]
+public class PhysicsSimulationEditor : Editor {
+
+	public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        PhysicsSimulation sim = (PhysicsSimulation)target;
+
+        if (GUILayout.Button("Simulate"))
+        {
+            sim.RunSimulation();
+        }
+
+		if (GUILayout.Button("Reset"))
+		{
+            sim.ResetAllBodies();
+		}
+    }
+
+}
